4d867f9f48285015ecf44d1bf55ba3e4fec54c10
xuanyuansen <lianhua@mogujie.com>
2016-08-02 13:57:20 +0800

    add python script to work with text data

---
+++ src/main/python/wordsembedding/util.py
@@ -0,0 +1,43 @@
+#coding=UTF-8
+'''
+Created on 2016年8月2日
+
+@author: wangshuai
+'''
+import gensim,logging
+import os
+import sys
+
+
+class SimpleSentences(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.idx=0
+
+    def __iter__(self):
+        for line in open(self.filename, 'r'):
+            try:
+                self.idx += 1
+                if self.idx % 500 ==0:
+                    print "iter: ",self.idx,line
+                yield "START,{0},END".format(line).split(",")
+            except:
+                print "illegal sentence"
+                yield [""]
+
+def trainword2vectormodel(datapath='dataFile', size=128, alpha=0.02, window=5, workers=8):
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    sentences = SimpleSentences(datapath) # a memory-friendly iterator
+    model = gensim.models.Word2Vec(sentences, size=size, alpha=alpha, window=window, workers=workers)
+    if not os.path.exists('./word2vec/'):
+        os.mkdir('./word2vec/')
+    model.save('./word2vec/Word2VecModel')
+
+
+if __name__ == '__main__':
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+    trainword2vectormodel(sys.argv[1])
+
+    pass
